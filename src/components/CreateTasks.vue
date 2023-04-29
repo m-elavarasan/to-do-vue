08c463f23c521ebcd4ae2fd384de20a3c5379e58
m-elavarasan <elasty2019@gmail.com>
2023-04-29 12:55:15 +0530

    Create Tasks and drag and drop fixed

---
+++ src/components/CreateTasks.vue
@@ -0,0 +1,37 @@
+<template>
+    <Dialog v-model:visible="visible" header="Create New" :style="{ width: '50vw' }">
+      <div class="p-fluid">
+        <div class="p-field p-grid">
+          <label for="title" class="p-col-fixed p-mb-2">Title</label>
+          <div class="p-col">
+            <InputText id="title" v-model="tasks.title" />
+          </div>
+        </div>
+        <div class="p-field p-grid">
+          <label for="description" class="p-col-fixed p-mt-2">Description</label>
+          <div class="p-col">
+            <InputText id="description" v-model="tasks.description" />
+          </div>
+        </div>
+      </div>
+      <div class="p-dialog-footer">
+        <Button label="Create" icon="pi pi-check" @click="handleCreate" />
+        <Button label="Cancel" icon="pi pi-times" class="p-button-secondary" @click="handleCancel" />
+      </div>
+    </Dialog>
+</template>
+
+<script>
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+    export default {
+        components: {
+    InputText,
+    Button
+  },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/ListComp.vue
+++ src/components/ListComp.vue
@@ -4,11 +4,9 @@
   </div>
   <div class="container">
     <div class="container_one" @dragover.prevent @drop="onDrop('created')">
-      <span>
-        <h2>Created</h2>
-      </span>
+      <span><h2>Created</h2></span>
       <div v-if="this.getTasks?.filter(task => task.status === 'created').length === 0" class="container_info">
-        <span class="pi pi-plus"> Create New Task</span>
+        <Button icon="pi pi-plus" label="Create New" @click="visible = true" />
       </div>
       <div v-else class="container_task">
         <div class="card-list">
@@ -17,6 +15,7 @@
             draggable="true" />
         </div>
       </div>
+
     </div>
     <div class="container_two" @dragover.prevent @drop="onDrop('started')">
       <span>
@@ -48,46 +47,107 @@
         </div>
       </div>
     </div>
+    <Dialog v-model:visible="visible" header="Create New" :style="{ width: '50vw' }">
+      <div class="p-fluid">
+        <div class="p-field p-grid">
+          <label for="title" class="p-col-fixed p-mb-2">Title</label>
+          <div class="p-col">
+            <InputText id="title" v-model="tasks.title" />
+          </div>
+        </div>
+        <div class="p-field p-grid">
+          <label for="description" class="p-col-fixed p-mt-2">Description</label>
+          <div class="p-col">
+            <InputText id="description" v-model="tasks.description" />
+          </div>
+        </div>
+      </div>
+      <div class="p-dialog-footer">
+        <Button label="Create" icon="pi pi-check" @click="handleCreate" />
+        <Button label="Cancel" icon="pi pi-times" class="p-button-secondary" @click="handleCancel" />
+      </div>
+    </Dialog>
   </div>
 </template>
 
 <script>
 import Card from '../components/CardComp.vue';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
 import { mapActions, mapGetters } from 'vuex';
 import firestoreService from "@/services/toDoService";
 
 export default {
   data() {
     return {
-      tasks: [
-        {
-          id: 0,
-          title: "Test Task",
-          description: "Sample task description",
-        }
-      ],
-      draggedTask: null
+      tasks:
+      {
+        id:"",
+        title: "",
+        description: "",
+        status: "created",
+      },
+      draggedTask: null,
+      visible: false,
     };
   },
   components: {
-    Card
+    Card,
+    Dialog,
+    InputText,
+    Button
   },
   mounted() {
     this.fetchTasks();
   },
   computed: {
-    ...mapGetters(['getTasks'])
+    ...mapGetters(['getTasks']),
+    isValid(){
+      if(this.tasks.title === "" && this.tasks.description === ""){
+          return false;
+        }
+      return true;
+    }
   },
   methods: {
     ...mapActions(['fetchTasks', 'updateTask']),
     onDragStart(task) {
       this.draggedTask = task;
     },
+    handleCreate() {
+      this.onCreateTasks(this.tasks)
+
+    },
+    handleCancel() {
+      this.resetTasks()
+      this.visible = false;
+    },
+    resetTasks() {
+      this.tasks.title = "",
+        this.tasks.description = ""
+    },
+    async onCreateTasks(tasks) {
+      if (this.isValid) {
+        await firestoreService.postData("tasks", tasks)
+          .then(() => {
+            this.fetchTasks();
+            console.log("Task updated successfully!");
+            this.resetTasks()
+            this.visible = false;
+          })
+          .catch(error => {
+            console.error("Error updating task in Firestore:", error);
+          });
+      }
+      else{
+        console.error("Invalid Input");
+      }
+    },
     async onDrop(status) {
       if (this.draggedTask) {
         this.draggedTask.status = status;
-        console.log(this.getTasks);
-      await firestoreService.updateData("tasks", this.draggedTask.id,this.draggedTask)
+        await firestoreService.updateData("tasks", this.draggedTask.id, this.draggedTask)
           .then(() => {
             this.draggedTask = null;
             console.log("Task updated successfully!");
@@ -106,6 +166,7 @@ export default {
   margin-top: 60px;
   display: flex;
   justify-content: center;
+  align-items: center;
 }
 
 .container {
@@ -170,4 +231,11 @@ export default {
   &_task {
     overflow: hidden;
   }
-}</style>
+}
+
+.p-dialog-footer {
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+}
+</style>
