376944afc6fc0ca9681f7ac23bc20dd6a8933e0a
ELAVARASAN M <elasty2019@gmail.com>
2023-04-30 15:14:33 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,12 @@
 export default {
-    apiKey: "AIzaSyAuggiSsbnR9m4WzJMYBKka9KQK84wmISc",
-  authDomain: "to-do-56cfb.firebaseapp.com",
-  projectId: "to-do-56cfb",
-  storageBucket: "to-do-56cfb.appspot.com",
-  messagingSenderId: "939897301984",
-  appId: "1:939897301984:web:03ca36525f104068de9985",
+    apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "9",
+  appId: "",
   firestore: {
-    timestampsInSnapshots: true, 
-    cacheSizeBytes: 1000000,
+    timestampsInSnapshots: , 
+    cacheSizeBytes: ,
   }
 };
